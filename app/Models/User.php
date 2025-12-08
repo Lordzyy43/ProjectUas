77fea0af4cf7b026f985ac363fca6c7553ca516f
Kang Tahu <kuri3275@gmailo.com>
2025-12-08 16:36:58 +0700

    backend complete

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,44 +2,29 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\Enrollment;
 use App\Models\UserQuizResult;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+       'name',
+       'email',
+       'password',
+       'role'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -47,7 +32,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-        public function enrollments()
+
+    public function enrollments()
     {
         return $this->hasMany(Enrollment::class);
     }
@@ -56,5 +42,4 @@ class User extends Authenticatable
     {
         return $this->hasMany(UserQuizResult::class);
     }
-
 }
